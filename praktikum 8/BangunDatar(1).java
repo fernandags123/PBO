d7fb52c09b254ee50847da103b5ae2301c7a4dac
fernandags123 <100529240+fernandags123@users.noreply.github.com>
2023-05-31 13:58:02 +0700

    Update BangunDatar(1).java

--- praktikum 8/BangunDatar(1).java
+++ praktikum 8/BangunDatar(1).java
@@ -1,6 +1,6 @@
 /**
  * File      : BangunDatar.java
- * Penulis   : Sulthan Firmansyah / 24060121140108
+ * Penulis   : Fernanda Galih Saputra / 24060121140176
  * Deskripsi : kelas abstrak untuk bangun datar
  *
  */
