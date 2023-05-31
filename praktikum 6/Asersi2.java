c80d3f0c087f5c83348e9e3e5fd3dd2dae161117
fernandags123 <100529240+fernandags123@users.noreply.github.com>
2023-05-31 13:42:55 +0700

    Update Asersi2.java

--- praktikum 6/Asersi2.java
+++ praktikum 6/Asersi2.java
@@ -1,6 +1,6 @@
 /*
 	Asersi2.java  30/03/2023
-    Nama / NIM    : Sulthan Firmansyah / 24060121140108
+    Nama / NIM    : Fernanda Galih Saputra / 24060121140176
 	Deskripsi     : Program untuk menujukkan asersi
 */
 
