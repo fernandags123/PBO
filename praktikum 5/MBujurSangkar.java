535165183681c9a80ddd6f5d042f4436fd933bac
fernandags123 <100529240+fernandags123@users.noreply.github.com>
2023-05-31 13:36:07 +0700

    Update MBujurSangkar.java

--- praktikum 5/MBujurSangkar.java
+++ praktikum 5/MBujurSangkar.java
@@ -1,6 +1,6 @@
 /**
 * File		 : MBujurSangkar.java 29-03-2023
-* Nama / NIM : Sulthan Firmansyah / 24060121140108
+* Nama / NIM : Fernanda Galih Saputra / 24060121140176
 * Deskripsi	 : Kelas yang mengimplementasikan cara menghitung luas bujur sangkar
 */
 
