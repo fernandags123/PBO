82ecb3b4d81c7e4f4ed0c185344df84f64c6626a
fernandags123 <100529240+fernandags123@users.noreply.github.com>
2023-05-31 13:35:13 +0700

    Update IArea.java

--- praktikum 5/IArea.java
+++ praktikum 5/IArea.java
@@ -1,6 +1,6 @@
 /**
 * File 		    : IArea.java 26-03-2023
-* Nama / NIM	: Sulthan Firmansyah / 24060121140108
+* Nama / NIM	: Fernanda Galih Saputra / 24060121140176
 * Deskripsi   	: Interface yang mendefinisikan kontrak bahwa setiap kelas 
 			      yang membuat implementasi dari interface ini harus memiliki cara
 			      untuk menghitung luas
