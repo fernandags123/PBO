23f7d1cfb4c6594f01cd6c7c1fbb857a6e76b34f
fernandags123 <100529240+fernandags123@users.noreply.github.com>
2023-05-31 13:58:28 +0700

    Update BangunDatarGeneric.java

--- praktikum 8/BangunDatarGeneric.java
+++ praktikum 8/BangunDatarGeneric.java
@@ -1,6 +1,6 @@
 /**
  * File      : BangunDatarGeneric.java
- * Penulis   : Sulthan Firmansyah / 24060121140108
+ * Penulis   : Fernanda Galih Saputra / 24060121140176
  * Deskripsi : kelas konstruksi generic untuk BangunDatar
  *
  */
