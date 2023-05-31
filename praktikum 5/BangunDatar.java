bdb6af7d6e807fe092c3039186feb8aa495ae613
fernandags123 <100529240+fernandags123@users.noreply.github.com>
2023-05-31 13:34:01 +0700

    Update BangunDatar.java

--- praktikum 5/BangunDatar.java
+++ praktikum 5/BangunDatar.java
@@ -1,6 +1,6 @@
 /**
 * File		    : BangunDatar.java 29-03-2023
-* Nama / NIM	: Sulthan Firmansyah / 24060121140108
+* Nama / NIM	: Fernanda Galih Saputra / 24060121140176
 * Deskripsi	    : Kelas abstrak, berisi abstraksi bangun datar
 */
 
