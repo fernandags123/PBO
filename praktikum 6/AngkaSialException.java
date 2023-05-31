032dbac42d5b5243d022c5635bd7fc492cbea126
fernandags123 <100529240+fernandags123@users.noreply.github.com>
2023-05-31 13:41:38 +0700

    Update AngkaSialException.java

--- praktikum 6/AngkaSialException.java
+++ praktikum 6/AngkaSialException.java
@@ -1,6 +1,6 @@
 /*
 	AngkaSialException.java  30/03/2023
-    Nama / NIM    : Sulthan Firmansyah / 24060121140108
+    Nama / NIM    : Fernanda Galih Saputra / 24060121140176
 	Deskripsi     : Eksepsi buatan sendiri, menolak masukan angka 13!
 */
 
